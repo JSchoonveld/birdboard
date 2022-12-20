8889c528e672c1994d6ddefcbb6e90191e82d509
Jesse Schoonveld <jschoonveld@LAPTOP-F5H98AO2.localdomain>
2022-12-20 08:29:54 +0100

    added breeze, wrote more store tests

--- app/Http/Requests/StoreProjectRequest.php
+++ app/Http/Requests/StoreProjectRequest.php
@@ -7,16 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class StoreProjectRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, mixed>
@@ -25,7 +15,8 @@ class StoreProjectRequest extends FormRequest
     {
         return [
             'title' => 'required|min:8',
-            'description' => 'required|min:8'
+            'description' => 'required|min:8',
+            'owner_id' => 'required'
         ];
     }
 }
